60339e25a45322afc240f0d164e992ffa4ec257a
Charlie Laver <charlielaver7@gmail.com>
2022-02-10 11:07:23 +0000

    env

--- bash_profile.sh
+++
@@ -1,9 +0,0 @@
-## to replace ~/.profile in ubuntu env
-## run: source ~/.profile after making changes
-
-## custom commands
-
-## search for a string in web dev project
-alias s="sudo egrep -ir --include=*.{php,html,js}"
-## open vs code as root
-alias sudo_vs="sudo code --user-data-dir='~/.vscode-root'"
